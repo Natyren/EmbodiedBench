0353a6b79210d7d4327371636831e6f308a037f7
Natyren <bredis.georgy@yandex.ru>
2025-03-11 16:49:42 +0300

    .

--- embodiedbench/envs/eb_alfred/scripts/startx.py
+++ embodiedbench/envs/eb_alfred/scripts/startx.py
@@ -1,99 +1,47 @@
 #!/usr/bin/env python
-
-
 import subprocess
 import shlex
-import re
 import platform
-import tempfile
-import os
 import sys
 
-def pci_records():
-    records = []
-    command = shlex.split('lspci -vmm')
-    output = subprocess.check_output(command).decode()
-
-    for devices in output.strip().split("\n\n"):
-        record = {}
-        records.append(record)
-        for row in devices.split("\n"):
-            key, value = row.split("\t")
-            record[key.split(':')[0]] = value
-
-    return records
-
-def generate_xorg_conf(devices):
-    xorg_conf = []
-
-    device_section = """
-Section "Device"
-    Identifier     "Device{device_id}"
-    Driver         "nvidia"
-    VendorName     "NVIDIA Corporation"
-    BusID          "{bus_id}"
-EndSection
-"""
-    server_layout_section = """
-Section "ServerLayout"
-    Identifier     "Layout0"
-    {screen_records}
-EndSection
-"""
-    screen_section = """
-Section "Screen"
-    Identifier     "Screen{screen_id}"
-    Device         "Device{device_id}"
-    DefaultDepth    24
-    Option         "AllowEmptyInitialConfiguration" "True"
-    SubSection     "Display"
-        Depth       24
-        Virtual 1024 768
-    EndSubSection
-EndSection
-"""
-    screen_records = []
-    for i, bus_id in enumerate(devices):
-        xorg_conf.append(device_section.format(device_id=i, bus_id=bus_id))
-        xorg_conf.append(screen_section.format(device_id=i, screen_id=i))
-        screen_records.append('Screen {screen_id} "Screen{screen_id}" 0 0'.format(screen_id=i))
-
-    xorg_conf.append(server_layout_section.format(screen_records="\n    ".join(screen_records)))
-
-    output =  "\n".join(xorg_conf)
-    print(output)
-    return output
-
-def startx(display):
+def start_xvfb(display, config_path=None):
     if platform.system() != 'Linux':
-        raise Exception("Can only run startx on linux")
+        raise Exception("Xvfb is only supported on Linux")
 
-    devices = []
-    for r in pci_records():
-        if r.get('Vendor', '') == 'NVIDIA Corporation' \
-                and r['Class'] in ['VGA compatible controller', '3D controller']:
-            bus_id = 'PCI:' + ':'.join(map(lambda x: str(int(x, 16)), re.split(r'[:\.]', r['Slot'])))
-            devices.append(bus_id)
-
-    if not devices:
-        raise Exception("no nvidia cards found")
+    # Build the command-line arguments.
+    # Note: Xvfb (which stands for X Virtual FrameBuffer) normally does not use a configuration file,
+    # so the '-config' option is not supported in most cases.
+    # Likewise, while you can pass extension toggles (like +extension GLX), these might be ignored
+    # or even rejected if your Xvfb build was not compiled with support for such extensions.
+    args = [
+        "Xvfb",
+        ":%d" % display,
+        "-noreset",
+        "+extension", "GLX",
+        "+extension", "RANDR",
+        "+extension", "RENDER",
+        "-screen", "0", "1024x768x24",
+    ]
     
+    # If (and only if) your build of Xvfb supports a configuration file (which most do not),
+    # you can add the -config parameter. In most cases you will simply omit this.
+    if config_path:
+        args.extend(["-config", config_path])
 
-    try:
-        fd, path = tempfile.mkstemp(dir='')
-        path = path.split('/')[-1]
-        with open(path, "w") as f:
-            f.write(generate_xorg_conf(devices))
-        command = shlex.split("Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config %s :%s" % (path, display))
-        subprocess.call(command)
-    finally:
-        os.close(fd)
-        os.unlink(path)
-
+    print("Starting Xvfb with command:", " ".join(args))
+    subprocess.call(args)
 
 if __name__ == '__main__':
+    # Use display 0 by default; optionally specify a display number and config file.
     display = 0
+    config_path = None
     if len(sys.argv) > 1:
-        display = int(sys.argv[1])
-    print("Starting X on DISPLAY=:%s" % display)
-    startx(display)
+        try:
+            display = int(sys.argv[1])
+        except ValueError:
+            sys.exit("Display must be an integer.")
+    if len(sys.argv) > 2:
+        config_path = sys.argv[2]
+
+    print("Starting Xvfb on DISPLAY=:%s" % display)
+    start_xvfb(display, config_path)
