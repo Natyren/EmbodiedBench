ff719ae177312d6dcdaff8a11672c8049384b157
Natyren <bredis.georgy@yandex.ru>
2025-03-11 16:28:04 +0300

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 source "$(conda info --base)/etc/profile.d/conda.sh"
 export EMBODIED_BENCH_ROOT=$(pwd)
 
+conda init 
+source ~/.bashrc
 
 # # Environment for ```Habitat and Alfred```
 conda env create -f conda_envs/environment.yaml 
@@ -11,11 +14,6 @@ pip install -e .
 # Environment for ```EB-Navigation```
 conda env create -f conda_envs/environment_eb-nav.yaml 
 conda activate embench_nav
-pip install -e .
-
-# Environment for ```EB-Manipulation```
-conda env create -f conda_envs/environment_eb-man.yaml 
-conda activate embench_man
 pip install -e .
 
 # Install Git LFS
@@ -37,26 +35,3 @@ cd ..
 conda install -y -c conda-forge git-lfs
 python -m habitat_sim.utils.datasets_download --uids rearrange_task_assets
 mv data embodiedbench/envs/eb_habitat
-
-# Install EB-Manipulation
-conda activate embench_man
-cd embodiedbench/envs/eb_manipulation
-wget https://downloads.coppeliarobotics.com/V4_1_0/CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
-tar -xf CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
-rm CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
-mv CoppeliaSim_Pro_V4_1_0_Ubuntu20_04/ $EMBODIED_BENCH_ROOT
-export COPPELIASIM_ROOT=$EMBODIED_BENCH_ROOT/CoppeliaSim_Pro_V4_1_0_Ubuntu20_04
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
-export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
-git clone https://github.com/stepjam/PyRep.git
-cd PyRep
-pip install -r requirements.txt
-pip install -e .
-cd ..
-pip install -r requirements.txt
-pip install -e .
-cp ./simAddOnScript_PyRep.lua $COPPELIASIM_ROOT
-git clone https://huggingface.co/datasets/EmbodiedBench/EB-Manipulation
-mv EB-Manipulation/data/ ./
-rm -rf EB-Manipulation/
-cd ../../..
